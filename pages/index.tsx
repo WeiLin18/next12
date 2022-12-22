2a86c2b5d75e7b092b043e118ca583e91b3d40f7
Iko Lin <iko@dacard.cc>
2022-12-22 14:40:41 +0800

    feat: add useRouter example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <Link href="/about">Go to the about page</Link>
+      <button type="button" onClick={() => router.push("/useRouter")}>
+        go to page by router push
+      </button>
+    </div>
   );
 }
---
+++ pages/useRouter.tsx
@@ -0,0 +1,3 @@
+export default function UseRouterPage() {
+  return <div>use router page</div>;
+}
